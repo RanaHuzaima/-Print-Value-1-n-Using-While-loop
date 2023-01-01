8e009bbfcd4ce088c5affada6faa0fdc0c03d0a8
Rana Huzaima <huzaimadigital@gmail.com>
2023-01-01 07:44:19 +0500

    First Commit

---
+++ while-Loop-Program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n,i=1;
+    cout<<"Enter a Value: ";
+    cin>>n;
+    while (i<=n)
+    {
+        cout<<i<<" ";
+        i=i+1;
+    }
+    return 0;
+
+}
